571f2628056778ea7e27006cde2e0c6c53ed676c
dijkstrabc <110527543+dijkstrabc@users.noreply.github.com>
2026-04-21 11:52:02 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,23 +1,11 @@
-from .qwen_node import QwenImageEditNode
-from .qwen_chat_node import QwenChatNode
-from .qwen_prompt_node import QwenPromptMasterNode
-from .qwen_t2i_node import QwenImageCreateNode
-from .qwen_repaint_node import QwenImageRepaintNode # 新增重绘节点
+from .qwen_node import QwenImageEditNode1
 
 NODE_CLASS_MAPPINGS = {
-    "QwenImageEditNode": QwenImageEditNode,
-    "QwenChatNode": QwenChatNode,
-    "QwenPromptMasterNode": QwenPromptMasterNode,
-    "QwenImageCreateNode": QwenImageCreateNode,
-    "QwenImageRepaintNode": QwenImageRepaintNode # 新增
+    "QwenImageEditNode1": QwenImageEditNode1,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "QwenImageEditNode": "Qwen Image Edit (Aliyun)",
-    "QwenChatNode": "Qwen Chat & VL (Aliyun)",
-    "QwenPromptMasterNode": "Qwen Prompt Master (Aliyun)",
-    "QwenImageCreateNode": "Qwen Image Create (Aliyun)",
-    "QwenImageRepaintNode": "Qwen Image Repaint (Aliyun)" # 新增显示名称
+    "QwenImageEditNode1": "Qwen Image Edit (Aliyun)1",
 }
 
 __all__ =['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
